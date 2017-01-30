9ec8eb6424a9704fe8079ee5939f0f53c40754c0
Clement Smith <cdsmith@umn.edu>
2017-01-30 11:20:12 -0600

    Update deletion confirmation process
    This removes one of the non-Javascript deletion confirmation pages
    and corrects calls to redirect() to prevent errors being thrown
    on deletion confirmation pages.

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_massaction'; // Required in Moodle 3.0.
-$plugin->version = 2016111700;  // YYYYMMDDXX (year, month, day, increment).
+$plugin->version = 2017013000;  // YYYYMMDDXX (year, month, day, increment).
 $plugin->requires = 2015111600; // YYYYMMDDXX (This is the release version for Moodle 3.0).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.0.1';
+$plugin->release = 'v3.0.2';
